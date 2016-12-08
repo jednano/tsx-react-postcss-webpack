7fdfc15fe98a47423f7e229b7610b8ec44024089
Jed Mao <jedmao@outlook.com>
2016-12-07 18:15:25 -0600

    Restore postcss processors

--- components/Home.js
+++ components/Home.js
@@ -10,13 +10,15 @@ export default React.createClass({
     render() {
         return (
             <BaseBody>
-                <h1>TypeScript/React/Webpack Starter</h1>
-                <ul>
-                    <li>
-                        <a href="/ts">Click for TypeScript Route</a>
-                    </li>
-                </ul>
-                <Foo bar="thud" />
+                <div className="home">
+                    <h1>TypeScript/React/Webpack Starter</h1>
+                    <ul>
+                        <li>
+                            <a href="/ts">Click for TypeScript Route</a>
+                        </li>
+                    </ul>
+                    <Foo bar="thud" />
+                </div>
             </BaseBody>
         );
     }
--- styles/config/postcss-processors.js
+++ styles/config/postcss-processors.js
@@ -1,130 +1,113 @@
 import { noop } from 'lodash';
 import { join } from 'path';
-// import * as postcssNested from 'postcss-nested';
-// import * as postcssNestedProps from 'postcss-nested-props';
-// import * as postcssPropertyLookup from 'postcss-property-lookup';
+import * as postcssNested from 'postcss-nested';
+import postcssNestedProps from 'postcss-nested-props';
+import * as postcssPropertyLookup from 'postcss-property-lookup';
 
-// import * as _postcssNestedVars from 'postcss-nested-vars';
-// import * as _postcssMixins from 'postcss-mixins';
-import * as _postcssImport from 'postcss-import';
-// import * as _postcssBemLinter from 'postcss-bem-linter';
-// import * as _postcssUrl from 'postcss-url';
-// import * as _postcssSprites from 'postcss-sprites';
-// import * as _postcssFontPack from 'postcss-font-pack';
-// import * as _postcssAllLinkColors from 'postcss-all-link-colors';
-// import * as _postcssCenter from 'postcss-center';
-// import * as _postcssCircle from 'postcss-circle';
-// import * as _postcssTriangle from 'postcss-triangle';
-// import * as _postcssClearfix from 'postcss-clearfix';
-// import * as _postcssCssNext from 'postcss-cssnext';
-// import * as _postcssBrowserReporter from 'postcss-browser-reporter';
-// import * as _cssWring from 'csswring';
-// import * as _postcssCopy from 'postcss-copy';
+import * as variables from './vars.json';
+import * as fonts from './fonts.json';
 
-// import * as variables from './vars.json';
-// import * as fonts from './fonts.json';
+const browsers = [
+    'last 2 versions',
+    'ie >= 9',
+    '> 1% in US'
+];
 
-// const browsers = [
-//     'last 2 versions',
-//     'ie >= 9',
-//     '> 1% in US'
-// ];
-
-// const postcssNestedVars = _postcssNestedVars({
-//     globals: variables
-// });
+const postcssNestedVars = require('postcss-nested-vars')({
+    globals: variables
+});
 
 export default function createPostcssProcessors(options) {
     options = options || {};
-    // const env = options.env || 'production';
-    // const { stylesheetPath, spritePath, groupBy } = options;
-    // const mixins = _postcssMixins({
-    //     mixinsFiles: join(__dirname, '..', 'mixins', '!(*.spec.js)')
-    // });
-    // const WORD = '[a-z0-9]+(-[a-z0-9]+)*';
+    const env = options.env || 'production';
+    const { stylesheetPath, spritePath, groupBy } = options;
+    const mixins = require('postcss-mixins')({
+        mixinsFiles: join(__dirname, '..', 'mixins', '!(*.spec.js)')
+    });
+    const WORD = '[a-z0-9]+(-[a-z0-9]+)*';
     const processors = [
-        // mixins,
-        // postcssPropertyLookup,
-        // postcssNestedProps,
-        // postcssNestedVars,
-        // postcssNested,
-        _postcssImport({
+        mixins,
+        postcssPropertyLookup,
+        postcssNestedProps,
+        postcssNestedVars,
+        postcssNested,
+        require('postcss-import')({
             path: [
                 'styles',
                 'styles/blocks'
             ],
             async: true,
             plugins: [
-                // mixins,
-                // postcssPropertyLookup,
-                // postcssNestedProps,
-                // postcssNestedVars,
-                // postcssNested,
-                // _postcssBemLinter({
-                //     componentName: new RegExp(`^${WORD}$`),
-                //     componentSelectors(block) {
-                //         const element = `(?:__${WORD})?`;
-                //         const modifier = `(?:--${WORD})?`;
-                //         const attributes = '(?:\\[[^\\]]*\\])*';
-                //         return new RegExp(`^(?:\\.${block}${element}${modifier}${attributes})+$`);
-                //     }
-                // })
+                mixins,
+                postcssPropertyLookup,
+                postcssNestedProps,
+                postcssNestedVars,
+                postcssNested,
+                require('postcss-bem-linter')({
+                    componentName: new RegExp(`^${WORD}$`),
+                    componentSelectors(block) {
+                        const element = `(?:__${WORD})?`;
+                        const modifier = `(?:--${WORD})?`;
+                        const attributes = '(?:\\[[^\\]]*\\])*';
+                        return new RegExp(`^(?:\\.${block}${element}${modifier}${attributes})+$`);
+                    }
+                })
             ],
             onImport: options.hot
                 ? files => files.forEach(this.addDependency)
                 : noop
         }),
-        // _postcssUrl,
-        // _postcssSprites({
-        //     stylesheetPath,
-        //     spritePath,
-        //     groupBy,
-        //     skipPrefix: true,
-        //     outputDimensions: true,
-        //     padding: 10,
-        //     verbose: true,
-        //     retina: true
-        // }),
-        // _postcssFontPack({
-        //     requireSize: true,
-        //     packs: fonts
-        // }),
-        // _postcssAllLinkColors,
-        // _postcssCenter,
-        // _postcssCircle,
-        // _postcssTriangle,
-        // _postcssClearfix,
-        // _postcssCssNext({
-        //     browsers,
-        //     features: {
-        //         customProperties: {
-        //             variables
-        //         },
-        //         rem: false
-        //     }
-        // })
+        require('postcss-url'),
+        require('postcss-sprites')({
+            stylesheetPath,
+            spritePath,
+            groupBy,
+            skipPrefix: true,
+            outputDimensions: true,
+            padding: 10,
+            verbose: true,
+            retina: true
+        }),
+        require('postcss-font-pack')({
+            requireSize: true,
+            packs: fonts
+        }),
+        require('postcss-all-link-colors'),
+        require('postcss-center'),
+        require('postcss-circle'),
+        require('postcss-triangle'),
+        require('postcss-clearfix'),
+        require('postcss-cssnext')({
+            browsers,
+            features: {
+                customProperties: {
+                    variables
+                },
+                rem: false
+            }
+        })
     ];
 
-    // if (env === 'development') {
-    //     processors.push(_postcssBrowserReporter);
-    // }
+    if (env === 'development') {
+        processors.push(require('postcss-browser-reporter'));
+    }
 
-    // if (env === 'production') {
-    //     processors.push(_cssWring);
-    //     processors.push(_postcssCopy({
-    //         src: 'build/public/',
-    //         dest: 'build/public/',
-    //         inputPath() {
-    //             return 'build/public/css';
-    //         },
-    //         relativePath(dirname, fileMeta, result, opts) {
-    //             return join(opts.dest, 'css');
-    //         },
-    //         template(fileMeta) {
-    //             return join(fileMeta.path, `${fileMeta.name}-${fileMeta.hash}.${fileMeta.ext}`);
-    //         }
-    //     }));
-    // }
+    if (env === 'production') {
+        processors.push(require('csswring'));
+        processors.push(require('postcss-copy')({
+            src: 'build/public/',
+            dest: 'build/public/',
+            inputPath() {
+                return 'build/public/css';
+            },
+            relativePath(dirname, fileMeta, result, opts) {
+                return join(opts.dest, 'css');
+            },
+            template(fileMeta) {
+                return join(fileMeta.path, `${fileMeta.name}-${fileMeta.hash}.${fileMeta.ext}`);
+            }
+        }));
+    }
 
     return processors;
 }
